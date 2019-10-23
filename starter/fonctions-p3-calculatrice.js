71932c88299363c1f85ec09000316b3443cbe9a7
Myriam Dupont <myriam.dupont@gmail.com>
2019-10-23 02:49:12 +0200

    added 2 prepas

--- starter/fonctions-p3-calculatrice.js
+++ starter/fonctions-p3-calculatrice.js
@@ -1,10 +1,13 @@
 /*
 FONCTIONS - PRÉPA 3 : Une première calculatrice
-1. Déclarez la fonction calculer() pour qu'elle gère les 4 opérations mathématiques de base :
+1. Déclarez la fonction calculer()
+	pour qu'elle gère les 4 opérations mathématiques de base :
 	addition, soustraction, multiplication et division.
+	Conseil : utilisez un switch pour basculer entre les différentes opérations.
 2. Utilisez ensuite votre fonction pour calculer
-	- 4 + 6
-	- 4 - 6
-	- 2 * 0
-	- 12 / 0
+	- 4 + 6 (qui doit évidemment donner 10)
+	- 4 - 6 (qui doit évidemment donner -2)
+	- 2 * 0 (qui doit évidemment donner 0)
+	- 12 / 0 (qui doit donner Infinity)
+	Aide : votre fonction s'exécute avec le pattern suivant : calculer(nb1, "+", nb2)
 */
